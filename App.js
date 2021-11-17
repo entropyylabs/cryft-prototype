fb3207436b8b09ad1a3146ae83d319388a303984
Ayush Jain <ayushjain2205@gmail.com>
2021-11-18 01:42:03 +0530

    Updated home screen wallet card

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import GlobalStyles from "./GlobalStyles";
+import HomeCard from "./HomeCard";
+import AppLoading from "expo-app-loading";
+import { useFonts, Khand_400Regular } from "@expo-google-fonts/khand";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    Khand_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <SafeAreaView style={GlobalStyles.droidSafeArea}>
+        <Text
+          style={{
+            fontFamily: "Khand_400Regular",
+            fontSize: 24,
+            color: "#72C0BD",
+            paddingTop: 10,
+            paddingLeft: 36,
+          }}
+        >
+          Hi Ishika
+        </Text>
+        <HomeCard />
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
   },
 });
---
+++ HomeCard.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Nunito_600SemiBold,
+  Nunito_300Light,
+} from "@expo-google-fonts/nunito";
+import { Khand_600SemiBold } from "@expo-google-fonts/khand";
+import { Ionicons } from "@expo/vector-icons";
+
+const HomeCard = () => {
+  let [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Khand_600SemiBold,
+    Nunito_300Light,
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <LinearGradient
+        // Background Linear Gradient
+        style={styles.card}
+        colors={["#13547A", "#80D0C7"]}
+        start={[0.0, 0.0]}
+        end={[1.0, 1.0]}
+      >
+        <View style={styles.card}>
+          <View style={styles.topRow}>
+            <Text style={styles.chipCount}>2 CHIPS</Text>
+            <Text style={styles.cryftLogo}>cryft</Text>
+          </View>
+
+          <View>
+            <Text style={styles.cardMessage}>
+              Zeppo just added 2 CHIPS to your wallet
+            </Text>
+          </View>
+
+          <View style={styles.walletIdButton}>
+            <Text style={styles.walletIdButtonText}>
+              <Ionicons name='md-barcode-sharp' size={18} color='white' />
+              &nbsp; View wallet ID
+            </Text>
+          </View>
+        </View>
+      </LinearGradient>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  card: {
+    display: "flex",
+    flexDirection: "column",
+    width: "95%",
+    alignSelf: "center",
+    height: 203,
+    borderRadius: 20,
+    marginTop: 12,
+  },
+  chipCount: {
+    color: "#ffffff",
+    fontSize: 24,
+    fontFamily: "Nunito_600SemiBold",
+  },
+  topRow: {
+    display: "flex",
+    flexDirection: "row",
+    padding: 20,
+    paddingBottom: 12,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  cryftLogo: {
+    fontFamily: "Khand_600SemiBold",
+    fontSize: 24,
+    color: "#185A7E",
+  },
+  cardMessage: {
+    color: "#ffffff",
+    fontFamily: "Nunito_300Light",
+    width: 158,
+    paddingLeft: 20,
+  },
+  walletIdButton: {
+    backgroundColor: "#000000",
+    height: 46,
+    width: 311,
+    borderRadius: 20,
+    marginTop: 25,
+    marginLeft: 12,
+    width: "98%",
+    display: "flex",
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  walletIdButtonText: {
+    display: "flex",
+    alignItems: "center",
+    color: "#ffffff",
+    fontFamily: "Nunito_300Light",
+    fontSize: 18,
+  },
+});
+
+export default HomeCard;
